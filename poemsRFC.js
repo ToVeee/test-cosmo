e91336f514f2e7c609210cf99536d859f1c47fc7
ToVeee <10bonito11@gmail.com>
2025-10-23 18:46:44 +0800

    Update poemsRFC.js

--- poemsRFC.js
+++ poemsRFC.js
@@ -1,14 +1,14 @@
 const poemsReference = {
   maningas_tagalog: "no item",
-  maningas_english: `I wonder if it drives them crazy
+  maningas_english: 'I wonder if it drives them crazy
 How do they manage to sleep at night?
 Knowing that many are suffering.
-All because of their <span class=”uf”    onclick=”play()
-   Alert(‘Having a desire to harm, annoy, or upset someone, often stemming from a feeling of being wronged.’)”> spite. 
+All because of their <span class="uf"   onclick=”play()
+   Alert(‘Having a desire to harm, annoy, or upset someone, often stemming from a feeling of being wronged.’)"> spite. 
   </span> Citizens work hard
 day and night,
 While those in power sit on their
- <span class=”uf”    onclick=”play()
+ <span class="uf"   onclick=”play()
    Alert(‘It is a physical and symbolic seat of power for a ruler, often elevated and grand, to emphasize their high status and governance.’)”>
    throne. 
   </span> Mindlessly overstepping our rights
@@ -17,7 +17,8 @@ As if they want us to <span class=”uf”    onclick=”play()
   offer our skin and bones. 
   </span>
 
-`,
+
+',
   maningas_sixword_t: `no item`,
   maningas_sixword_e": `nk item`
 };
