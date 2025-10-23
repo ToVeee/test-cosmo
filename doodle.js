f0f334423c7eaa6a9884572b04745c52cf62b6b7
ToVeee <10bonito11@gmail.com>
2025-10-24 06:41:39 +0800

    Update doodle.js

--- doodle.js
+++ doodle.js
@@ -47,6 +47,7 @@ function showPage() {
     img.style.border = "1px solid #ccc";
     img.style.width = "200px";
     img.style.width = "200px";
+    img.classList.add = "doodles"
     img.style.marginBottom = "20px"; // fixed typo
     img.style.borderRadius = "6px";
     sec2.appendChild(img);
@@ -67,4 +68,5 @@ document.getElementById("next").addEventListener("click", () => {
 
 // Initial load
 showPage();
+
 
