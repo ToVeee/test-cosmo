8a0b4f1daf40b3f95d2c550cf64bc9eb17f18d45
ToVeee <10bonito11@gmail.com>
2025-10-23 12:16:03 +0800

    Update rfc.js

--- rfc.js
+++ rfc.js
@@ -1,330 +1,38 @@
+// rfc.js
 const studentslist = {
 
-"Almacen, Prince Noah Balamban": {
-  PFP: "almacen.png",
-  TagalogPoem: "Almacen, poem tagalog.docx",
-  EnglishPoem: "Almacen, poem english.docx",
-  SixWordE: "Almacen, ff e.docx",
-  SixWordT: "Almacen tagalog.docx"
-},
+  "Almacen, Prince Noah Balamban": {
+    PFP: "almacen.png",
+    TagalogPoem: "Almacen, poem tagalog.docx",
+    EnglishPoem: "Almacen, poem english.docx",
+    SixWordE: "Almacen, ff e.docx",
+    SixWordT: "Almacen tagalog.docx"
+  },
 
-"Almariego, Brisbane Rojo": {
-  PFP: "Almariego, Brisbane R_.png",
-  TagalogPoem: "Almariego, poem tagalog.docx",
-  EnglishPoem: "Almariego, poem english.docx",
-  SixWordE: "Almariego ff e.docx",
-  SixWordT: "Almariego tagalog.docx"
-},
+  "Almariego, Brisbane Rojo": {
+    PFP: "Almariego, Brisbane R_.png",
+    TagalogPoem: "Almariego, poem tagalog.docx",
+    EnglishPoem: "Almariego, poem english.docx",
+    SixWordE: "Almariego ff e.docx",
+    SixWordT: "Almariego tagalog.docx"
+  },
 
-"Bunyi, Dhave Zairhon Rosales": {
-  PFP: "Bunyi, Dhave Zairhon R_.png",
-  TagalogPoem: "Bunyi, poem tagalog.docx",
-  EnglishPoem: "Bunyi, poem english.docx",
-  SixWordE: "Bunyi, ff e.docx",
-  SixWordT: "Bunyi, ff tagalog.docx",
-  poem: `adi`
-},
+  "Bunyi, Dhave Zairhon Rosales": {
+    PFP: "Bunyi, Dhave Zairhon R_.png",
+    TagalogPoem: "Bunyi, poem tagalog.docx",
+    EnglishPoem: "Bunyi, poem english.docx",
+    SixWordE: "Bunyi, ff e.docx",
+    SixWordT: "Bunyi, ff tagalog.docx"
+  },
 
-"Chavez, Dancris Madriaga": {
-  PFP: "Chavez, Dancris M_.png",
-  TagalogPoem: "Chavez, poem tagalog.docx",
-  EnglishPoem: "Chavez, poem english.docx",
-  SixWordE: "Chavez ff e.docx",
-  SixWordT: "Chavez tagalog.docx"
-},
+  "Chavez, Dancris Madriaga": {
+    PFP: "Chavez, Dancris M_.png",
+    TagalogPoem: "Chavez, poem tagalog.docx",
+    EnglishPoem: "Chavez, poem english.docx",
+    SixWordE: "Chavez ff e.docx",
+    SixWordT: "Chavez tagalog.docx"
+  },
 
-"Eguia, Ej Jhanver Almirez": {
-  PFP: "Eguia, EJ Jhanver A.png",
-  TagalogPoem: "Eguia, poem tagalog.docx",
-  EnglishPoem: "Eguia, poem english.docx",
-  SixWordE: "Eguia ff e.docx",
-  SixWordT: "Eguia tagalog.docx"
-},
-
-"Jarabejo, Janoah Sereño": {
-  PFP: "Jarabejo, Janoah S_.png",
-  TagalogPoem: "Jarabejo, poem tagalog.docx",
-  EnglishPoem: "Jarabejo, poem english.docx",
-  SixWordE: "Jarabejo ff e.docx",
-  SixWordT: "Jarabejo tagalog.docx"
-},
-
-"Manalastas, Roniel James Mendoza": {
-  PFP: "Manalastas, Roniel James M_.png",
-  TagalogPoem: "Manalastas, poem tagalog.docx",
-  EnglishPoem: "Manalastas, poem english.docx",
-  SixWordE: "Manalastas ff e.docx",
-  SixWordT: "Manalastas tagalog.docx"
-},
-
-"Manipol, John Bernard Llamas": {
-  PFP: "Manipol, John Bernard Ll.png",
-  TagalogPoem: "Manipol, poem tagalog.docx",
-  EnglishPoem: "Manipol, poem english.docx",
-  SixWordE: "Manipol ff e.docx",
-  SixWordT: "Manipol tagalog.docx"
-},
-
-"Manuba, Heinrich Datos": {
-  PFP: "Manuba, Henrich_.png",
-  TagalogPoem: "Manuba, poem tagalog.docx",
-  EnglishPoem: "Manuba, poem english.docx",
-  SixWordE: "Manuba ff e.docx",
-  SixWordT: "Manuba tagalog.docx"
-},
-
-"Nabor, Aaron Josh Callejo": {
-  PFP: "Nabor, Aaron Josh C_.png",
-  TagalogPoem: "Nabor, poem tagalog.docx",
-  EnglishPoem: "Nabor, poem english.docx",
-  SixWordE: "Nabor ff e.docx",
-  SixWordT: "Nabor tagalog.docx"
-},
-
-"Pasatiempo, Ram Manalastas": {
-  PFP: "Pasatiempo, Ram M_.png",
-  TagalogPoem: "Pasatiempo, poem tagalog.docx",
-  EnglishPoem: "Pasatiempo, poem english.docx",
-  SixWordE: "Pasatiempo ff e.docx",
-  SixWordT: "Pasatiempo tagalog.docx"
-},
-
-"Revillame, Christian Tadalan": {
-  PFP: "Revillame, Christian T_.png",
-  TagalogPoem: "Revillame, poem tagalog.docx",
-  EnglishPoem: "Revillame, poem english.docx",
-  SixWordE: "Revillame ff e.docx",
-  SixWordT: "Revillame tagalog.docx"
-},
-
-"Timog, Keeno Mccain Urgelles": {
-  PFP: "Timog, Keeno Mccain U_.png",
-  TagalogPoem: "Timog, poem tagalog.docx",
-  EnglishPoem: "Timog, poem english.docx",
-  SixWordE: "Timog ff e.docx",
-  SixWordT: "Timog tagalog.docx"
-},
-
-"Torres, Karl Kristian Evangelista": {
-  PFP: "Torres, Karl Kristian E_.png",
-  TagalogPoem: "Torres, poem tagalog.docx",
-  EnglishPoem: "Torres, poem english.docx",
-  SixWordE: "Torres ff e.docx",
-  SixWordT: "Torres tagalog.docx"
-},
-
-"Umali, Leonel Inaanuran": {
-  PFP: "Umali, Leonel I_.png",
-  TagalogPoem: "Umali, poem tagalog.docx",
-  EnglishPoem: "Umali, poem english.docx",
-  SixWordE: "Umali ff e.docx",
-  SixWordT: "Umali tagalog.docx"
-},
-
-"Villanueva, Zian Lee Almoneda": {
-  PFP: "Villanueva,Zian Lee A_.png",
-  TagalogPoem: "Villanueva, poem tagalog.docx",
-  EnglishPoem: "Villanueva, poem english.docx",
-  SixWordE: "Villanueva ff e.docx",
-  SixWordT: "Villanueva tagalog.docx"
-},
-
-"Banagan, Jena Mae Ramos": {
-  PFP: "Banagan, Jena May R_.png",
-  TagalogPoem: "Banagan, poem tagalog.docx",
-  EnglishPoem: "Banagan, poem english.docx",
-  SixWordE: "Banagan ff e.docx",
-  SixWordT: "Banagan ff t.docx"
-},
-
-"Buela, Carla Mae Igna": {
-  PFP: "Buela, Carla Mae I_.png",
-  TagalogPoem: "Buela, poem tagalog.docx",
-  EnglishPoem: "Buela, poem english.docx",
-  SixWordE: "Buela ff e.docx",
-  SixWordT: "Buela ff t.docx"
-},
-
-"Calong, Martha Joanna Morilla": {
-  PFP: "Calong, Martha Joanna M_.png",
-  TagalogPoem: "Calong, poem tagalog.docx",
-  EnglishPoem: "Calong, poem english.docx",
-  SixWordE: "Calong ff e.docx",
-  SixWordT: "Calong ff t.docx"
-},
-
-"Calucin, Jellah Mae Manaog": {
-  PFP: "Calucin.png",
-  TagalogPoem: "Calucin, poem tagalog.docx",
-  EnglishPoem: "Calucin, poem english.docx",
-  SixWordE: "Calucin ff e.docx",
-  SixWordT: "Calucin ff t.docx"
-},
-
-"Clarito, Leexanne Gabriel Alfonso": {
-  PFP: "Clarito, Leexanne Gabriel A_.png",
-  TagalogPoem: "Clarito, poem tagalog.docx",
-  EnglishPoem: "Clarito, poem english.docx",
-  SixWordE: "Clarito ff e.docx",
-  SixWordT: "Clarito ff t.docx"
-},
-
-"Colito, Sarah Austria": {
-  PFP: "Colito,Sarah A_.png",
-  TagalogPoem: "Colito, poem tagalog.docx",
-  EnglishPoem: "Colito, poem english.docx",
-  SixWordE: "Colito ff e.docx",
-  SixWordT: "Colito ff t.docx"
-},
-
-"Dela Peña, Cyrah Pradel": {
-  PFP: "Dela Peña, Cyrah P_.png",
-  TagalogPoem: "Dela Peña ... og.docx",
-  EnglishPoem: "Dela Peña ... sh.docx",
-  SixWordE: "Dela Peña, ff e.docx",
-  SixWordT: "Dela Peña, ff t.docx"
-},
-
-"Empensando, Reanna Noreen Romero": {
-  PFP: "Empensando, Reanna Noreen R_.png",
-  TagalogPoem: "Empensando ... g.docx",
-  EnglishPoem: "Empensando ... h.docx",
-  SixWordE: "Empensando ... e.docx",
-  SixWordT: "Empensando ... t.docx"
-},
-
-"Encontro, Angel Anne Villaseñor": {
-  PFP: "Encontro, Angel Anne V_.png",
-  TagalogPoem: "Encontro ... og.docx",
-  EnglishPoem: "Encontro ... sh.docx",
-  SixWordE: "Encontro, ff e.docx",
-  SixWordT: "Encontro, ff t.docx"
-},
-
-"Gratuito, Rizza Mae Almacen": {
-  PFP: "Gratuito, Rizza Mae Almacen_.png",
-  TagalogPoem: "Gratuito ... log.docx",
-  EnglishPoem: "Gratuito ... ish.docx",
-  SixWordE: "Gratuito, ff e.docx",
-  SixWordT: "Gratuito, ff t.docx"
-},
-
-"Iemura, Sayuri De Los Angeles": {
-  PFP: "Iemura, Sayuri D_.png",
-  TagalogPoem: "Iemura, poem tagalog.docx",
-  EnglishPoem: "Iemura, poem english.docx",
-  SixWordE: "Iemura, ff e.docx",
-  SixWordT: "Iemura, ff t.docx"
-},
-
-"Ilarde, Hariette Manlutac": {
-  PFP: "Ilarde, Hariette M_.png",
-  TagalogPoem: "Ilarde, poem tagalog.docx",
-  EnglishPoem: "Ilarde, poem english.docx",
-  SixWordE: "Ilarde, ff e.docx",
-  SixWordT: "Ilarde, ff t.docx"
-},
-
-"Latre, Jazel Cabusao": {
-  PFP: "Latre, Jazel C.png",
-  TagalogPoem: "Latre, poem tagalog.docx",
-  EnglishPoem: "Latre, poem english.docx",
-  SixWordE: "Latre, ff e.docx",
-  SixWordT: "Latre, ff t.docx"
-},
-
-"Lechuga, Carla Jane Sinapilo": {
-  PFP: "Lechuga, Carla Jane S_.png",
-  TagalogPoem: "Lechuga, poem tagalog.docx",
-  EnglishPoem: "Lechuga, poem english.docx",
-  SixWordE: "Lechuga, ff e.docx",
-  SixWordT: "Lechuga, ff t.docx"
-},
-
-"Leron, Trixie Anne Gaon": {
-  PFP: "Leron,Trixie Anne G_.png",
-  TagalogPoem: "Leron, poem tagalog.docx",
-  EnglishPoem: "Leron, poem english.docx",
-  SixWordE: "Leron, ff e.docx",
-  SixWordT: "Leron, ff t.docx"
-},
-
-"Maningas, Dannah Mae Cubalan": {
-  PFP: "Maningas, Dannah Mae C_.png",
-  TagalogPoem: "Maningas, poem tagalog.docx",
-  EnglishPoem: maningas_english,
-  SixWordE: "Maningas, ff e.docx",
-  SixWordT: "Maningas, ff t.docx"
-},
-
-"Nilooban, Jade Ann Sobria": {
-  PFP: "Nilooban,Jade Ann S_.png",
-  TagalogPoem: "Nilooban, poem tagalog.docx",
-  EnglishPoem: "Nilooban, poem english.docx",
-  SixWordE: "Nilooban, ff e.docx",
-  SixWordT: "Nilooban, ff t.docx"
-},
-
-"Ostonal, Akisha Rachelle Therese": {
-  PFP: "akisha.png",
-  TagalogPoem: "Ostonal, poem tagalog.docx",
-  EnglishPoem: "Ostonal, poem english.docx",
-  SixWordE: "Ostonal, ff e.docx",
-  SixWordT: "Ostonal, ff t.docx"
-},
-
-"Palma, Creancie Kence Almacen": {
-  PFP: "Palma, Creancie kence A_.png",
-  TagalogPoem: "Palma, poem tagalog.docx",
-  EnglishPoem: "Palma, poem english.docx",
-  SixWordE: "Palma, ff e.docx",
-  SixWordT: "Palma, ff t.docx"
-},
-
-"Pintor, Jillian Neille Aumentado": {
-  PFP: "Pintor, Jillian Neille A_.png",
-  TagalogPoem: "Pintor, poem tagalog.docx",
-  EnglishPoem: "Pintor, poem english.docx",
-  SixWordE: "Pintor, ff e.docx",
-  SixWordT: "Pintor, ff t.docx"
-},
-
-"Rivera, Jendy Faith Derecho": {
-  PFP: "Rivera, Jendy Faith D_.png",
-  TagalogPoem: "Rivera, poem tagalog.docx",
-  EnglishPoem: "Rivera, poem english.docx",
-  SixWordE: "Rivera, ff e.docx",
-  SixWordT: "Rivera, ff t.docx"
-},
-
-"Rubio, Rhiana Cathleen Fernandez": {
-  PFP: "Rubio,Rhianna Cathleen F_.png",
-  TagalogPoem: "Rubio, poem tagalog.docx",
-  EnglishPoem: "Rubio, poem english.docx",
-  SixWordE: "Rubio, ff e.docx",
-  SixWordT: "Rubio, ff t.docx"
-},
-
-"Teves, Melaiza Lardizabal": {
-  PFP: "Teves, Melaiza L_.png",
-  TagalogPoem: "Teves, poem tagalog.docx",
-  EnglishPoem: "Teves, poem english.docx",
-  SixWordE: "Teves, ff e.docx",
-  SixWordT: "Teves, ff t.docx"
-},
-
-"Urgelles, Donna May Donato": {
-  PFP: "Urgelles, Donna May D_.png",
-  TagalogPoem: "Urgelles, poem tagalog.docx",
-  EnglishPoem: "Urgelles, poem english.docx",
-  SixWordE: "Urgelles, ff e.docx",
-  SixWordT: "Urgelles, ff t.docx"
-}
-
+  // … Continue all the rest exactly as you listed …
+  
 };
-
-
-
-
-
-
