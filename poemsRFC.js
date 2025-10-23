f0d2eb1d53450d1ed8b3c19b6bc148de2d6cd095
ToVeee <10bonito11@gmail.com>
2025-10-23 09:17:27 +0800

    Create poemsRFC.js

---
+++ poemsRFC.js
@@ -0,0 +1,23 @@
+const poemsReference = {
+  "maningas_tagalog": "no item",
+  "maningas_english": `I wonder if it drives them crazy
+How do they manage to sleep at night?
+Knowing that many are suffering.
+All because of their <span class=”uf”    onclick=”play()
+   Alert(‘Having a desire to harm, annoy, or upset someone, often stemming from a feeling of being wronged.’)”> spite. 
+  </span> Citizens work hard
+day and night,
+While those in power sit on their
+ <span class=”uf”    onclick=”play()
+   Alert(‘It is a physical and symbolic seat of power for a ruler, often elevated and grand, to emphasize their high status and governance.’)”>
+   throne. 
+  </span> Mindlessly overstepping our rights
+As if they want us to <span class=”uf”    onclick=”play()
+   Alert(‘To give up something valuable or to destroy something in a ritualistic manner for a higher purpose, such as pleasing a god, seeking atonement, or demonstrating spiritual discipline.  ‘)”>
+  offer our skin and bones. 
+  </span>
+
+`,
+  "maningas_sixword_t": `no item`,
+  "maningas_sixword_e": `nk item`
+};
